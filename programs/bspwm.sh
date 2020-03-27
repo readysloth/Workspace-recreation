46fe639cda2bdde41773bc8d5b3a762329a9c5bd
readysloth <readysloth@protonmail.com>
2020-03-27 17:35:48 +0000

    Доделал скрипт для bspwm

--- programs/bspwm.sh
+++ programs/bspwm.sh
@@ -4,64 +4,50 @@ download(){
 
 download_dependencies(){
     pushd bspwm
-        git clone https://gitlab.freedesktop.org/xorg/lib/libxcb.git &
-        git clone https://gitlab.freedesktop.org/xorg/proto/xcbproto.git &
-	git clone --recursive https://gitlab.freedesktop.org/xorg/lib/libxcb-util.git &
-	wait $(jobs -p)
+    	mkdir dependencies
+	pushd dependencies
+            git clone https://gitlab.freedesktop.org/xorg/lib/libxcb.git &
+            git clone https://gitlab.freedesktop.org/xorg/proto/xcbproto.git &
+	    git clone --recursive https://gitlab.freedesktop.org/xorg/lib/libxcb-util.git &
+	    git clone --recursive https://gitlab.freedesktop.org/xorg/lib/libxcb-wm.git &
+	    git clone --recursive https://gitlab.freedesktop.org/xorg/lib/libxcb-keysyms.git &
+	    wait $(jobs -p)
+	popd
     popd
 }
 
 install_dependencies(){
     pushd bspwm
-        sudo apt-get -y install \
-                            autoconf \
-                            automake \
-                            pkg-config \
-			    xutils-dev \
-			    libtool
-	pushd xcbproto
-            sh autogen.sh
-	    make -j$(nproc)
-	    sudo make install
-	popd
-
-	pushd libxcb-util
-            sh autogen.sh
-	    make -j$(nproc)
-	    sudo make install
-	popd
-
-	pushd libxcb
-	    sh autogen.sh
-	    make -j$(nproc)
-	    sudo make install
+	pushd dependencies
+            sudo apt-get -y install \
+                                autoconf \
+                                automake \
+                                pkg-config \
+	    		        libtool
+	    for dependency in $(ls)
+	    do
+	        pushd $dependency
+                    sh autogen.sh
+	            make -j$(nproc)
+	            sudo make install
+	        popd
+	    done
 	popd
     popd
+    echo "/usr/local/lib" | sudo tee /etc/ld.so.conf.d/another-lib-folder.conf
+    sudo ldconfig
 }
 
 download_misc(){
-    pushd st
-        mkdir st_patches
-        pushd st_patches
-
-            #wget https://st.suckless.org/patches/hidecursor/st-hidecursor-0.8.1.diff
-            wget https://github.com/juliusHuelsmann/st/releases/download/patchesV3/st-alphaFocusHighlight-20200216-26cdfeb.diff
-
-        popd
-    popd
+    echo
 }
 
 install_misc(){
-    pushd st
-        for patch in $(ls st_patches); do
-            git apply -3 st_patches/$patch
-        done
-    popd
+    echo
 }
 
 install(){
-    pushd st
-        sed -i '/static char *font =/ s/pixelsize=../pixelsize=16/' config.h
+    pushd bspwm
         make -j$(nproc)
         sudo make install
     popd
