0cda0da8487a22d979dc27a3737435c2e1fecaf7
readysloth <readysloth@protonmail.com>
2020-06-24 20:22:21 +0300

    Добавил создание ext4 разделов. Косметические правки

--- partitioning.sh
+++ partitioning.sh
@@ -15,33 +15,36 @@ DISK_PART3="${DISK}3"
 
 LVM_GROUP_NAME="vg01"
 
-wipefs -a "$DISK"
+wipefs -a "${DISK}"
 
 EXISTING_LVM_GROUPS="$(vgs | sed -n 2,\$p | awk '{print $1}')"
 vgremove -y "${EXISTING_LVM_GROUPS}"
 
-parted -a optimal --script "$DISK" 'mklabel gpt'
-parted -a optimal --script "$DISK" 'mkpart primary 1MiB 3MiB'
-parted -a optimal --script "$DISK" 'name 1 grub'
-parted -a optimal --script "$DISK" 'set 1 bios_grub on'
+parted -a optimal --script "${DISK}" 'mklabel gpt'
+parted -a optimal --script "${DISK}" 'mkpart primary 1MiB 3MiB'
+parted -a optimal --script "${DISK}" 'name 1 grub'
+parted -a optimal --script "${DISK}" 'set 1 bios_grub on'
 
-parted -a optimal --script "$DISK" 'mkpart primary 3MiB 131MiB'
-parted -a optimal --script "$DISK" 'name 2 boot'
-parted -a optimal --script "$DISK" 'set 2 boot on'
+parted -a optimal --script "${DISK}" 'mkpart primary 3MiB 131MiB'
+parted -a optimal --script "${DISK}" 'name 2 boot'
+parted -a optimal --script "${DISK}" 'set 2 boot on'
 
-parted -a optimal --script "$DISK" 'mkpart primary 131MiB -1'
-parted -a optimal --script "$DISK" 'name 3 lvm01'
-parted -a optimal --script "$DISK" 'set 3 lvm on'
+parted -a optimal --script "${DISK}" 'mkpart primary 131MiB -1'
+parted -a optimal --script "${DISK}" 'name 3 lvm01'
+parted -a optimal --script "${DISK}" 'set 3 lvm on'
 
-print_if_verbatim parted -a optimal --script "$DISK" 'print'
+print_if_verbatim parted -a optimal --script "${DISK}" 'print'
 
 /etc/init.d/lvm start
-pvcreate "$DISK_PART3"
+pvcreate "${DISK_PART3}"
 
 print_if_verbatim pvdisplay
 
-vgcreate vg01 "$DISK_PART3"
+vgcreate vg01 "${DISK_PART3}"
 
-lvcreate -L 1024M -n swap "$LVM_GROUP_NAME"
-lvcreate -l 20%FREE -n rootfs "$LVM_GROUP_NAME"
-lvcreate -l 80%FREE -n home "$LVM_GROUP_NAME"
+lvcreate -L 1024M -n swap     "${LVM_GROUP_NAME}"
+lvcreate -l 20%FREE -n rootfs "${LVM_GROUP_NAME}"
+lvcreate -l 80%FREE -n home   "${LVM_GROUP_NAME}"
+
+mkfs.ext4 /dev/"${LVM_GROUP_NAME}"/rootfs
+mkfs.ext4 /dev/"${LVM_GROUP_NAME}"/home
