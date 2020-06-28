a95bd8df74bc8b33f93338e190e22f8ee11bc920
readysloth <readysloth@protonmail.com>
2020-06-28 15:24:04 +0300

    Поменял выделение памяти на партиции рута и хомяка, начал добавлять шаги установки.

--- installation.sh
+++ installation.sh
@@ -14,3 +14,15 @@ mount /dev/sda2 /boot
 emerge-webrsync
 emerge --sync
 emerge --oneshot sys/apps/portage
+
+eselect profile list
+
+printf 'select profile: '
+read profile_choice
+
+eselect profile set "${profile_choice}"
+
+eselect profile list
+
+emerge --update --deep --newuse @world
+emerge sys-kernel/gentoo-sources
--- partitioning.sh
+++ partitioning.sh
@@ -43,8 +43,8 @@ print_if_verbatim pvdisplay
 vgcreate vg01 "${DISK_PART3}"
 
 lvcreate -y -L 1024M   -n swap   "${LVM_GROUP_NAME}"
-lvcreate -y -l 30%FREE -n rootfs "${LVM_GROUP_NAME}"
-lvcreate -y -l 70%FREE -n home   "${LVM_GROUP_NAME}"
+lvcreate -y -l 40%FREE -n rootfs "${LVM_GROUP_NAME}"
+lvcreate -y -l 60%FREE -n home   "${LVM_GROUP_NAME}"
 
 mkfs.fat -F 32 "${DISK_PART2}"
 mkfs.ext4 /dev/"${LVM_GROUP_NAME}"/rootfs
