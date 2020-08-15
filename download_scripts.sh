03b0efa1200d1096483f416504aa834b05e4dbb9
readysloth <readysloth@protonmail.com>
2020-08-15 11:06:07 +0300

    Добавил переподклчение в скачивание скриптов

--- download_scripts.sh
+++ download_scripts.sh
@@ -2,13 +2,12 @@ BASE_URL='https://raw.githubusercontent.com/readysloth/Workspace-recreation/gent
 
 mkdir installation
 pushd installation
-    wget "${BASE_URL}/compiling.sh"
-    wget "${BASE_URL}/configuring.sh"
-    wget "${BASE_URL}/download_scripts.sh"
-    wget "${BASE_URL}/installation.sh"
-    wget "${BASE_URL}/partitioning.sh"
-    wget "${BASE_URL}/preinstallation.sh"
-
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/compiling.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/configuring.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/download_scripts.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/installation.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/partitioning.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/preinstallation.sh"
     chmod +x *
 popd
 
