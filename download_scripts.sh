c5d8ccd6ff14e5d5434dec7a8557ce9a428cf4ff
readysloth <readysloth@protonmail.com>
2020-10-31 14:24:39 +0300

    feat: изменил download_scripts.sh

--- download_scripts.sh
+++ download_scripts.sh
@@ -1,18 +1,19 @@
 set -o errexit
 
-BASE_URL='https://raw.githubusercontent.com/readysloth/Workspace-recreation/gentoo'
+BASE_URL='https://raw.githubusercontent.com/readysloth/Workspace-recreation/gentoo_py'
 
 mkdir installation
 pushd installation
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/compiling.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/configuring.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/download_scripts.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/installation.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/partitioning.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/preinstallation.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/with_tmpfs.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/create_configs.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/stage3.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/compiling.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/configuring.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/download_scripts.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/installation.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/partitioning.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/preinstallation.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/with_tmpfs.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/create_configs.py"
     chmod +x *
 popd
 
