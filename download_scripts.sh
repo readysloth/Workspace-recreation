ea2af66658180bb1c52ea551122cb1d04dd7aaac
readysloth <readysloth@protonmail.com>
2020-10-31 18:00:05 +0300

    feat: Изменил download_scripts

--- download_scripts.sh
+++ download_scripts.sh
@@ -2,18 +2,19 @@ set -o errexit
 
 BASE_URL='https://raw.githubusercontent.com/readysloth/Workspace-recreation/gentoo_py'
 
+
+
 mkdir installation
 pushd installation
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/create_configs.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/polybar_chooser.sh"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/stage3.sh"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/compiling.py"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/configuring.py"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/download_scripts.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/installation.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/partitioning.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/preinstallation.py"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/with_tmpfs.py"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.py"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/create_configs.py"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/utils.py"
     chmod +x *
 popd
 
