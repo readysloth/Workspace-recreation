8fc5808be470ca09fa2e0bd940a1753e8bc9c085
readysloth <readysloth@protonmail.com>
2020-11-03 09:01:25 +0300

    fix: добавил environment_install.py

--- download_scripts.sh
+++ download_scripts.sh
@@ -7,9 +7,9 @@ BASE_URL='https://raw.githubusercontent.com/readysloth/Workspace-recreation/gent
 mkdir installation
 pushd installation
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/create_configs.sh"
-    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.sh"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/polybar_chooser.sh"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/stage3.sh"
+    wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/environment_install.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/compiling.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/installation.py"
     wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 "${BASE_URL}/partitioning.py"
